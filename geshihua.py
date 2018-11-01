8021bc20b6569cd6fef6fe1790046f343db3edb0
huangll <1076127377@qq.com>
2018-11-01 17:17:37 +0800

    learning python

---
+++ geshihua.py
@@ -0,0 +1,26 @@
+print('hello,%s' % 'heihei')
+print('hello,%s,you konw you are my baby! %d' % ('jinjin',520))
+
+s1 = 72
+s2 = 85
+r = ((s2-s1)/s1)*100
+print('小明成绩提升了 %.2f %%' % r)
+
+#list 有序的集合
+classmate=['a','b','c']
+print(classmate)
+print(classmate[0])   #取第一个元素
+print(classmate[-1])   # 取最后一个元素，倒数第二个是-2
+classmate.append('d') #在最后一个添加
+classmate.append(3)
+print(classmate)
+classmate.insert(0,'aa')   #指定位置添加元素
+print(classmate)
+classmate.pop()   #删除最后一位
+classmate.pop(0)   #删除指定位
+print(classmate)
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,7 @@
+a=int(input('please enter the first number:'))
+b=int(input('please enter the last number:'))
+print('a+b=',a+b)
+# print('''line1
+# line2
+# line3''')
+
